dd0ca13c208b6d5f4e56d27fe2bd6437051f18b6
laber <807778564@qq.com>
2016-09-28 19:12:35 +0800

    创建主界面

---
+++ Umbrella/Main/MainController.h
@@ -0,0 +1,13 @@
+//
+//  MainController.h
+//  Umbrella
+//
+//  Created by huangrensheng on 16/9/28.
+//  Copyright © 2016年 umbrella. All rights reserved.
+//
+
+#import "BaseController.h"
+
+@interface MainController : BaseController
+
+@end
---
+++ Umbrella/Main/MainController.m
@@ -0,0 +1,42 @@
+//
+//  MainController.m
+//  Umbrella
+//
+//  Created by huangrensheng on 16/9/28.
+//  Copyright © 2016年 umbrella. All rights reserved.
+//
+
+#import "MainController.h"
+
+@interface MainController ()
+
+@end
+
+@implementation MainController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
